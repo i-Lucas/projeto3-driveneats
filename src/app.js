a9c3144e434c684bf25637d524b09ab849a31939
Lucas <novo.contato.lucas@gmail.com>
2022-01-29 19:38:36 -0300

    added new features

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ let border = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 3px #32B7
 let ListItemsNames = []
 let ListItemsPrice = []
 let TotalPrice = 0
-
 let total = []
-
 let selectedItems = []
 
 function dishes_selected(item) {
@@ -20,12 +18,10 @@ function dishes_selected(item) {
     // getting the name of the selected item
     let selectedNameItem = item.getElementsByTagName('h2')
     ListItemsNames[0] = selectedNameItem[0].innerHTML
-    // alert(ListItemsNames)
 
     // getting the value of the selected item
     let selectedPriceItem = item.getElementsByClassName('item-price')
-    ListItemsPrice[0] = Number(selectedPriceItem[0].innerHTML.replace(',','.'))
-    // alert(ListItemsPrice[0])
+    ListItemsPrice[0] = Number(selectedPriceItem[0].innerHTML.replace(',', '.'))
 
     selectedItems[0] = true
 
@@ -47,12 +43,10 @@ function drinks_selected(item) {
     // getting the name of the selected item
     let selectedNameItem = item.getElementsByTagName('h2')
     ListItemsNames[1] = selectedNameItem[0].innerHTML
-    // alert(ListItemsNames)
 
     // getting the value of the selected item
     let selectedPriceItem = item.getElementsByClassName('item-price')
-    ListItemsPrice[1] = Number(selectedPriceItem[0].innerHTML.replace(',','.'))
-    // alert(ListItemsPrice[0])
+    ListItemsPrice[1] = Number(selectedPriceItem[0].innerHTML.replace(',', '.'))
 
     selectedItems[1] = true
 
@@ -74,12 +68,10 @@ function desserts_selected(item) {
     // getting the name of the selected item
     let selectedNameItem = item.getElementsByTagName('h2')
     ListItemsNames[2] = selectedNameItem[0].innerHTML
-    // alert(ListItemsNames)
 
     // getting the value of the selected item
     let selectedPriceItem = item.getElementsByClassName('item-price')
-    ListItemsPrice[2] = Number(selectedPriceItem[0].innerHTML.replace(',','.'))
-    // alert(ListItemsPrice)
+    ListItemsPrice[2] = Number(selectedPriceItem[0].innerHTML.replace(',', '.'))
 
     selectedItems[2] = true
 
@@ -94,42 +86,90 @@ function closeOrder() {
     button.style.backgroundColor = '#32B72F';
     button.innerHTML = 'Fechar pedido'
 
-    TotalPrice = (ListItemsPrice[0]+ListItemsPrice[1]+ListItemsPrice[2]).toFixed(2)
-    alert(TotalPrice)
+    TotalPrice = (ListItemsPrice[0] + ListItemsPrice[1] + ListItemsPrice[2]).toFixed(2)
 }
 
 function infoOrder() {
-    if (ListItemsNames[0] == undefined && ListItemsNames[1] == undefined && ListItemsNames[2] == undefined) {
-        return alert("Você deve escolher seu pedido primeiramente !")
-    }
-    if(selectedItems[0] == undefined) {
-        return alert("Você deve escolher seu prato !")
-    }
-    if(selectedItems[1] == undefined) {
-        return alert("Você deve escolher sua bebida !")
-    }
-    if(selectedItems[2] == undefined) {
-        return alert("Você deve escolher sua sobremesa !")
-    }
 
+    if (OrderCheck()) {
+
+        // background-white
+        document.querySelector('.background-white').style.display = 'flex'
+
+        // remove footer button
+        document.getElementById('btn').classList.add('hidden')
+
+        // green confirmation screen
+        let hidden = document.querySelector(".order-confirm")
+        hidden.classList.remove("hidden");
+
+        // order data
+        let dishesName = []
+        let dishesPrice = []
+
+        dishesName[0] = document.querySelector('.order-name-0')
+        dishesName[0].innerHTML = ListItemsNames[0]
+        dishesPrice[0] = document.querySelector('.order-price-0')
+        dishesPrice[0].innerHTML = ListItemsPrice[0]
+
+        dishesName[1] = document.querySelector('.order-name-1')
+        dishesName[1].innerHTML = ListItemsNames[1]
+        dishesPrice[1] = document.querySelector('.order-price-1')
+        dishesPrice[1].innerHTML = ListItemsPrice[1]
+
+        dishesName[2] = document.querySelector('.order-name-2')
+        dishesName[2].innerHTML = ListItemsNames[2]
+        dishesPrice[2] = document.querySelector('.order-price-2')
+        dishesPrice[2].innerHTML = ListItemsPrice[2]
+
+        let totalOrderPrice = document.querySelector('.order-total-price')
+        totalOrderPrice.innerHTML = TotalPrice;
+    }
+}
+
+function confirmData() {
+    
     let userName = prompt('Por favor, informe-nos seu nome:')
     let userAddress = prompt('Para finalizarmos, informe seu endereço: ')
-    let userInfo= [userName,userAddress]
+    let userInfo = [userName, userAddress]
     sendOrder(userInfo)
 }
 
 function sendOrder(userInfo) {
-   
+
     const message = 'Olá, gostaria de fazer o pedido:'
-    +'\n - Prato: '     +   ListItemsNames[0]
-    +'\n - Bebida: '    +   ListItemsNames[1]
-    +'\n - Sobremesa: ' +   ListItemsNames[2]
-    +'\n - Total: R$: ' +   TotalPrice
-    
-    +'\n\n Nome: '      +   userInfo[0]
-    +'\n Endereço: '     +   userInfo[1];
+        + '\n - Prato: ' + ListItemsNames[0]
+        + '\n - Bebida: ' + ListItemsNames[1]
+        + '\n - Sobremesa: ' + ListItemsNames[2]
+        + '\n - Total: R$: ' + TotalPrice
 
-    const url = 'https://wa.me/5571999925706?text='+encodeURIComponent(message)
+        + '\n\n Nome: ' + userInfo[0]
+        + '\n Endereço: ' + userInfo[1];
 
-    window.open(url,'_blank')
+    const url = 'https://wa.me/5571999925706?text=' + encodeURIComponent(message)
+
+    window.open(url, '_blank')
+}
+
+function OrderCheck() {
+
+    if (ListItemsNames[0] == undefined && ListItemsNames[1] == undefined && ListItemsNames[2] == undefined) {
+        return alert("Você deve escolher seu pedido primeiramente !")
+    }
+    if (selectedItems[0] == undefined) {
+        return alert("Você deve escolher seu prato !")
+    }
+    if (selectedItems[1] == undefined) {
+        return alert("Você deve escolher sua bebida !")
+    }
+    if (selectedItems[2] == undefined) {
+        return alert("Você deve escolher sua sobremesa !")
+    }
+    else return true
+}
+
+function cancelOrder() {
+    document.querySelector('.background-white').style.display = 'none'
+    document.querySelector(".order-confirm").classList.add("hidden");
+    document.getElementById('btn').classList.remove('hidden')
 }
